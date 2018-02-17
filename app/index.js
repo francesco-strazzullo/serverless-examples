a9cbd1d30b2df10f20a90b8be7938cc416c374e4
francesco-strazzullo <francensco.strazzullo86@gmail.com>
2018-02-17 10:37:10 +0100

    Updates

--- app/index.js
+++
@@ -1,1 +0,0 @@
-window.fetch(`${BASE_URL}/api`).then(r => r.text()).then(data => document.write(data))
--- server/start.js
+++
@@ -1,3 +0,0 @@
-const app = require('./index')
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
